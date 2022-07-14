1a221dd98035968cadc91533072045412c323473
Joseggr <grangel.2769@unimar.edu.ve>
2022-07-14 00:35:59 -0400

    vista y controlador de calendario

---
+++ app/Http/Controllers/UsuariosController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UsuariosController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        if(!Auth::check()){
+            return redirect("login")->withSuccess('You are not allowed to access');
+        }
+  
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('usuarios');
+    }
+}
---
+++ resources/views/calendario.blade.php
@@ -0,0 +1,8 @@
+@include('header')
+<div class="container">
+    <h4 class="mb-4">Calendario</h4>
+    <div class="table-responsive">
+        
+    </div>
+</div>
+@include('footer')
